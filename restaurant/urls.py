6086c745d372053780343f1bba2eb990c2863b19
Hassan Lamhioui <hlamhioui@gmail.com>
2024-11-19 23:36:58 +0100

    Istall mysqlclient and django_filter dependencies

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -6,15 +6,18 @@ from . import views
 
 router = DefaultRouter(trailing_slash=False)
 router.register("users", views.UserViewSet, basename="user")
-router.register("menu-items", views.MenuViewSet, basename="menu-items")
-router.register("bookings", views.BookingViewSet, basename="bookings")
 
 urlpatterns = [
     path("",include(router.urls)),
     path("home/", views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+    path('reservations/', views.reservations, name="reservations"),
+
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api-token-auth/", obtain_auth_token),
-    path('about/', views.about, name="about"),
+
     path('menu/',views.menu,name="menu"),
     path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),
+    # path('bookings/', views.bookings, name='bookings'),
 ]
